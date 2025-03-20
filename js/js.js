93cc009afa25f928c45d83263f5db52af895a83f
reem-abdelrazek20 <156788337+reem-abdelrazek20@users.noreply.github.com>
2025-03-20 20:40:01 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -22,4 +22,44 @@ document.getElementById("menu-toggle").addEventListener("click", function () {
   });
 
 
+  document.addEventListener("DOMContentLoaded", function () {
+    let userInput = document.getElementById("name");
+    let emailInput = document.getElementById("Email-Address");
+    let numberInput = document.getElementById("Mobile-Number");
+    let errorName = document.getElementById("errorrname");
+    let errorEmail = document.getElementById("errorremail");
+  
+    document.forms[0].onsubmit = function (e) {
+      let userValid = true;
+      let emailValid = true;
+  
+      //  التحقق من الاسم
+      if (userInput.value.trim() === "") {
+        userValid = false;
+        errorName.classList.remove("hidden");
+       
+        userInput.style.border = "1px solid red";
+      } else {
+        errorName.classList.add("hidden");
+        userInput.style.border = "";
+      }
+  
+      //  التحقق من البريد الإلكتروني
+      if (emailInput.value.trim() === "" || !emailInput.value.includes("@")) {
+        emailValid = false;
+        errorEmail.classList.remove("hidden");
+        errorEmail.innerText = "Enter a valid email address";
+        emailInput.style.border = "1px solid red";
+      } else {
+        errorEmail.classList.add("hidden");
+        emailInput.style.border = "";
+      }
+  
+      //منع الإرسال إذا كان هناك أخطاء
+      if (!userValid || !emailValid) {
+        e.preventDefault();
+      }
+    };
+  });
+  
 
